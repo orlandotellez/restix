52d2020377cf562a1d438eaadfb921333d0ee8af
orlandotellez <espadachinkinno@gmail.com>
2026-03-22 19:22:49 -0600

    feat: renderizar las vistas y el mapeado de las teclas

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,110 @@
-fn main() {
-    println!("Hello, world!");
+mod controllers;
+mod views;
+
+use std::io::{self, Stdout};
+
+use anyhow::Result;
+use crossterm::{
+    event::{self, DisableMouseCapture, EnableMouseCapture, Event, KeyEventKind},
+    execute,
+    terminal::{EnterAlternateScreen, LeaveAlternateScreen, disable_raw_mode, enable_raw_mode},
+};
+use ratatui::{Terminal, prelude::CrosstermBackend};
+
+use crate::{
+    controllers::keyboard::{KeyboardController, NavigationAction},
+    views::{
+        key_info_view::KeyInfoView, keys_list_view::KeysListView, layout::MainLayout,
+        value_view::ValueView,
+    },
+};
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum FocusPanel {
+    KeysList,
+    Details,
+    Value,
+}
+
+pub struct App {
+    pub terminal: Terminal<CrosstermBackend<Stdout>>,
+
+    // Views
+    pub keys_list_view: KeysListView,
+    pub key_info_view: KeyInfoView,
+    pub value_view: ValueView,
+
+    pub last_key_name: String,
+    pub auto_refresh: bool,
+    pub last_refresh_time: std::time::Instant,
+}
+
+impl App {
+    pub fn new() -> Result<Self> {
+        Ok(Self {
+            terminal: Terminal::new(CrosstermBackend::new(io::stdout()))?,
+            keys_list_view: KeysListView::new(),
+            key_info_view: KeyInfoView::new(),
+            value_view: ValueView::new(),
+            last_key_name: String::new(),
+            auto_refresh: true,
+            last_refresh_time: std::time::Instant::now(),
+        })
+    }
+
+    pub fn run(&mut self) -> Result<()> {
+        // habilitamos modo raw(significa capturar teclas individuales)
+        enable_raw_mode()?;
+
+        // crear una pantalla nueva, no modificar la terminal actual
+        let mut stdout: Stdout = io::stdout();
+
+        execute!(stdout, EnterAlternateScreen, EnableMouseCapture)?;
+
+        let backend = CrosstermBackend::new(stdout);
+        let mut terminal = Terminal::new(backend)?;
+
+        loop {
+            terminal.draw(|frame| {
+                let area = frame.area();
+                let layout = MainLayout::new(area);
+
+                let (keys_list_area, key_info_area, value_area) =
+                    layout.split_panels(layout.main_content);
+
+                self.keys_list_view.render(frame, keys_list_area);
+                self.key_info_view.render(frame, key_info_area);
+                self.value_view.render(frame, value_area);
+            });
+
+            if event::poll(std::time::Duration::from_millis(100))? {
+                if let Event::Key(key) = event::read()? {
+                    if key.kind == KeyEventKind::Press {
+                        let action = KeyboardController::map_key_event(key);
+
+                        match action {
+                            NavigationAction::Quit => break,
+                            _ => {}
+                        }
+                    }
+                }
+            }
+        }
+
+        disable_raw_mode()?;
+
+        execute!(
+            terminal.backend_mut(),
+            LeaveAlternateScreen,
+            DisableMouseCapture
+        )?;
+        Ok(())
+    }
+}
+
+fn main() -> Result<()> {
+    let mut app = App::new()?;
+    app.run()?;
+
+    Ok(())
 }
